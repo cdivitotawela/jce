f98c03b36aa7fb9ae51ce990f2412d1127168cd3
Chaminda Divitotawela <cdivitotawela@console.com.au>
2018-05-14 12:06:40 +1000

    initial commit

---
+++ src/main/java/cloud/base2/jenkins/App.java
@@ -0,0 +1,13 @@
+package cloud.base2.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
